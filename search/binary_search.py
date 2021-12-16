ff376d801f3c5847e23caefd9b20cbd649f384ff
JeongwonShin <jennyshin106@gmail.com>
2021-12-16 15:42:22 +0900

    add: binary_search algorithm template

---
+++ search/binary_search.py
@@ -0,0 +1,20 @@
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] == target:
+            return mid
+        if array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
+
+n = 10
+target = 13
+array = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+
+result = binary_search(array, target, 0, n-1)
+if result == None:
+    print(None)
+else:
+    print(result+1)
