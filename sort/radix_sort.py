4c8836f071d566b6ba627a19ba06c94d8ac7f3d0
JeongwonShin <jennyshin106@gmail.com>
2021-12-16 20:54:04 +0900

    add: template for radix, selection sort

---
+++ sort/radix_sort.py
@@ -0,0 +1,11 @@
+array = [7, 4, 9, 0, 3, 1, 6, 2, 9, 1, 4, 8, 0, 5, 2]
+
+count = [0] * (max(array) + 1)
+
+for i in range(len(array)):
+    count[array[i]] += 1
+
+for i in range(len(count)):
+    for j in range(count[i]):
+        # 등장한 횟수만큼 프린트 
+        print(i, end=' ') 
