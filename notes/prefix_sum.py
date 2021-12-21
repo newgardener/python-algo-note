d92bfa46f1100dc36849072efef04a6bd29f794d
JeongwonShin <jennyshin106@gmail.com>
2021-12-21 10:34:03 +0900

    solve: implementation gamedev problem

--- notes/prefix_sum.py
+++ notes/prefix_sum.py
@@ -1,10 +1,12 @@
+#%%
 def get_prefix_sum(data, left, right):
     sum_value = 0
-    prefix_sum = [9]
+    prefix_sum = [0]
     
     for i in data:
         sum_value += i
         prefix_sum.append(sum_value)
+    print(prefix_sum)
         
     return prefix_sum[right] - prefix_sum[left-1]
 
@@ -13,3 +15,4 @@ left = 3
 right = 4
 print(get_prefix_sum(data, left, right))
     
+# %%
