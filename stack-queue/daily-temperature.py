762c7d92df589d810b209359a3bde8c882f6205e
JeongwonShin <jennyshin106@gmail.com>
2021-12-28 14:03:44 +0900

    solve: stack-queue problem
    - dailyTemperature

---
+++ stack-queue/daily-temperature.py
@@ -0,0 +1,13 @@
+T = [73, 74, 75, 71, 69, 72, 76, 73]
+
+def dailyTemperature(T):
+    answer = [0] * len(T)
+    stack = []
+    for i, cur in enumerate(T):
+        while stack and cur > T[stack[-1]]:
+            last = stack.pop()
+            answer[last] = i - last
+        stack.append(i)
+    return answer
+
+print(dailyTemperature(T))
