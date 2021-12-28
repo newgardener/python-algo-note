79614c9258ba91c2cb7713cd43e4cdc5178c2472
JeongwonShin <jennyshin106@gmail.com>
2021-12-28 20:27:11 +0900

    solve: greedy problems - max_profit & queue-reconstruction-by-height

---
+++ greedy/max_profit.py
@@ -0,0 +1,3 @@
+def maxProfit(prices):
+    # 0보다 크면 무조건 합산 
+    return sum(max(prices[i+1]-prices[i], 0) for i in range(len(prices) - 1))
---
+++ greedy/queue-reconstruction-by-height.py
@@ -0,0 +1,27 @@
+#%% solution 1
+import copy
+
+people = [[7, 0], [4, 4], [7, 1], [5, 0], [6, 1], [5, 2]]
+
+people.sort(key=lambda x: x[0], reverse=True)
+result = []
+
+for person in people:
+    result.insert(person[1], person)
+
+print(result)
+
+#%% solution 2 - using heap (priority queue)
+import heapq
+
+heap = []
+
+for person in people:
+    heapq.heappush(heap, (-person[0], person[1]))
+
+result = []
+while heap:
+    person = heapq.heappop(heap)
+    result.insert(person[1], [-person[0], person[1]])
+
+print(result)
