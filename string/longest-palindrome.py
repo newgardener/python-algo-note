b9939035e413a9ef43d3a288e14db33bf478199c
JeongwonShin <jennyshin106@gmail.com>
2024-04-09 00:29:52 +0900

    solve: easy version longestPalindrome

--- string/longest-palindrome.py
+++ string/longest-palindrome.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 def longestPalindrome(s):
     def expand(left, right):
         while left >= 0 and right < len(s) and s[left] == s[right]:
@@ -13,3 +16,22 @@ def longestPalindrome(s):
         result = max(result, expand(i, i + 1), expand(i, i + 2), key=len)
 
     return result
+
+
+def longestPalindromeEasy(s):
+    counter = Counter(s)
+
+    length = 0
+    oddNumberFound = False
+
+    for value in counter.values():
+        if value % 2 == 0:
+            length += value
+        else:
+            length += value - 1
+            oddNumberFound = True
+
+    if oddNumberFound:
+        length += 1
+
+    return length
