908de76ef1eee25ce87dd644aa5b601783c67861
JeongwonShin <jennyshin106@gmail.com>
2024-04-20 15:45:21 +0900

    feat: implement left-most, right-most binary search

--- search/binary_search.py
+++ search/binary_search.py
@@ -1,4 +1,8 @@
-def binary_search(array, target, start, end):
+# %%
+
+
+def binary_search(array, target):
+    start, end = 0, len(array) - 1
     while start <= end:
         mid = (start + end) // 2
         if array[mid] == target:
@@ -10,12 +14,41 @@ def binary_search(array, target, start, end):
     return None
 
 
-n = 10
-target = 13
-array = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+def leftmost_binary_search(array, target):
+    start, end = 0, len(array) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        # this ensures even if target is found, we can continue searching to the left to find first occurrence target
+        if array[mid] >= target:
+            end = mid - 1
+        else:
+            start = mid + 1
+        print(start, end, mid)
 
-result = binary_search(array, target, 0, n - 1)
-if result == None:
-    print(None)
-else:
-    print(result + 1)
+    if start < len(array) and array[start] == target:
+        return start
+    return None
+
+
+def rightmost_binary_search(array, target):
+    start, end = 0, len(array) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] > target:
+            end = mid - 1
+        # array[mid] <= start skips the current matched target to continue searching to the right to find last occurrence target
+        else:
+            start = mid + 1
+        print(start, end, mid)
+
+    if end >= 0 and array[end] == target:
+        return end
+    return None
+
+
+# %%
+target = 5
+array = [1, 3, 5, 5, 7, 9, 11]
+
+print(leftmost_binary_search(array, target))
+print(rightmost_binary_search(array, target))
