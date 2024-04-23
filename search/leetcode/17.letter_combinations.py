7fcf5bcace5ead8ef14030983ce1c1c08a1e27ee
JeongwonShin <jennyshin106@gmail.com>
2024-04-24 01:12:52 +0900

    feat: better solution for letterCombination problem

--- search/leetcode/17.letter_combinations.py
+++ search/leetcode/17.letter_combinations.py
@@ -35,3 +35,32 @@ def letterCombinations(digits: str) -> list[str]:
 
 
 print(letterCombinations("23"))
+
+
+def betterSolution(digits: str) -> list[str]:
+    letterMap = {
+        "2": "abc",
+        "3": "def",
+        "4": "ghi",
+        "5": "jkl",
+        "6": "mno",
+        "7": "pqrs",
+        "8": "tuv",
+        "9": "wxyz",
+    }
+
+    def dfs(build_string, remaining_string):
+        if not remaining_string:
+            result.append(build_string)
+            return
+
+        for letter in letterMap[remaining_string[0]]:
+            dfs(build_string + letter, remaining_string[1:])
+
+    result = []
+    dfs("", digits)
+
+    return result
+
+
+print(betterSolution("23"))
