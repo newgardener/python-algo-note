7438281a124be0ef88f32dacda84f65f262e7b39
JeongwonShin <jennyshin106@gmail.com>
2024-05-05 17:44:27 +0900

    solve: implement trie problem

---
+++ graph/leetcode/208-implement_trie.py
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/implement-trie-prefix-tree
+
+
+class Trie:
+    class Node:
+        def __init__(self):
+            self.children = {}
+            self.isEnd = False
+
+    def __init__(self):
+        self.root = Trie.Node()
+
+    def insert(self, word: str):
+        node = self.root
+        for ch in word:
+            if ch not in node.children:
+                node.children[ch] = Trie.Node()
+            node = node.children[ch]
+        node.isEnd = True
+
+    def search(self, word: str):
+        node = self.root
+        for ch in word:
+            if ch not in node.children:
+                return False
+            node = node.children[ch]
+        return node.isEnd
+
+    def startsWith(self, prefix: str):
+        node = self.root
+        for ch in prefix:
+            if ch not in node.children:
+                return False
+            node = node.children[ch]
+        return True
