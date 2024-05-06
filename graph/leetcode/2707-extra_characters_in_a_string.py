6b2df621136f3e39b672886c106c2f8973d2e790
JeongwonShin <jennyshin106@gmail.com>
2024-05-06 14:04:35 +0900

    solve: apply dp to solve extra characters in a string
    - forward version
    - backward version

--- graph/leetcode/2707-extra_characters_in_a_string.py
+++ graph/leetcode/2707-extra_characters_in_a_string.py
@@ -18,29 +18,50 @@ class Trie:
 
 
 class Solution:
-    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+    def minExtraCharBackward(self, s: str, dictionary: list[str]) -> int:
         trie = Trie()
         for word in dictionary:
             trie.insert(word)
 
-        start, result = 0, 0
+        n = len(s)
+        dp = [0] * (n + 1)
 
-        while start < len(s):
-            curr = trie.root
-            longest_match = -1
+        for start in range(n - 1, -1, -1):
+            dp[start] = dp[start + 1] + 1
 
-            for i in range(start, len(s)):
-                ch = s[i]
-                if ch not in curr.children:
-                    break
-                curr = curr.children[ch]
-                if curr.isEnd:
-                    longest_match = i
+            node = trie.root
+            for end in range(start, n):
+                if "a" <= s[end] <= "z":
+                    if s[end] not in node.children:
+                        break
+                    node = node.children[s[end]]
+                    if node.isEnd:
+                        dp[start] = min(dp[start], dp[end + 1])
+        return dp[0]
 
-            if longest_match == -1:
-                result += 1
-                start += 1
-            else:
-                start = longest_match + 1
+    def minExtraCharForward(self, s: str, dictionary: list[str]) -> int:
+        n = len(s)
+        dp = [float("inf")] * (n + 1)
+        words = set(dictionary)
+        dp[0] = 0
 
-        return result
+        for i in range(1, n + 1):
+            for word in words:
+                j = len(word)
+                if i >= j and s[i - j : i] == word:
+                    dp[i] = min(dp[i], dp[i - j])
+            dp[i] = min(dp[i], dp[i - 1] + 1)
+        return dp[n]
+
+
+solution = Solution()
+
+s1 = "leetscode"
+dictionary1 = ["leet", "code", "leetcode"]
+print(solution.minExtraCharForward(s1, dictionary1))
+print(solution.minExtraCharBackward(s1, dictionary1))
+
+s2 = "sayhelloworld"
+dictionary2 = ["hello", "world"]
+print(solution.minExtraCharForward(s2, dictionary2))
+print(solution.minExtraCharBackward(s2, dictionary2))
