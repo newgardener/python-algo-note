ae2d93fc31bfb269bb6606c6fa89f34256a2657c
JeongwonShin <jennyshin106@gmail.com>
2024-05-23 01:17:49 +0900

    enhance: answer into a O(n) time complexity

--- search/leetcode/2540-minimum_common_value.py
+++ search/leetcode/2540-minimum_common_value.py
@@ -3,20 +3,14 @@ from typing import List
 
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        def binarySearch(left, right, target):
-            while left <= right:
-                mid = (left + right) // 2
-                if nums1[mid] == target:
-                    return target
-                elif nums1[mid] > target:
-                    right = mid - 1
-                else:
-                    left = mid + 1
-            return -1
+        i, j = 0, 0
+        n1, n2 = len(nums1), len(nums2)
 
-        left, right = 0, len(nums1) - 1
-        for target in nums2:
-            result = binarySearch(left, right, target)
-            if result != -1:
-                return result
+        while i < n1 and j < n2:
+            if nums1[i] == nums2[j]:
+                return nums1[i]
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else:
+                j += 1
         return -1
