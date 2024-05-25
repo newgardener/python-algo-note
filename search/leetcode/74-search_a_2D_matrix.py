58258bf0ebe11b6219e2ed17743e2221bb2497c3
JeongwonShin <jennyshin106@gmail.com>
2024-05-25 17:40:43 +0900

    solve: matrix ii problem

--- search/leetcode/74-search_a_2D_matrix.py
+++ search/leetcode/74-search_a_2D_matrix.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/search-a-2d-matrix
+# https://leetcode.com/problems/search-a-2d-matrix-ii
 """
 Time Complexity: O(m+n) if m = # of row, n = # of column
 """
@@ -6,7 +7,7 @@ Time Complexity: O(m+n) if m = # of row, n = # of column
 from typing import List
 
 
-def searchMatrix(matrix: List[List[int]], target: int) -> bool:
+def searchMatrixI(matrix: List[List[int]], target: int) -> bool:
     if not matrix or not matrix[0]:
         return False
 
@@ -22,3 +23,38 @@ def searchMatrix(matrix: List[List[int]], target: int) -> bool:
         else:
             col -= 1
     return False
+
+
+def searchMatrixII(matrix: List[List[int]], target: int) -> bool:
+    def binarySearch(left, right, arr):
+        while left <= right:
+            mid = (left + right) // 2
+            if arr[mid] == target:
+                return mid
+            elif arr[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return right if right >= 0 else -1
+
+    colLeft, colRight = 0, len(matrix[0]) - 1
+    colIndex = binarySearch(colLeft, colRight, matrix[0])
+
+    while colIndex >= 0:
+        rowLeft, rowRight = 0, len(matrix) - 1
+        rowIndex = binarySearch(rowLeft, rowRight, [item[colIndex] for item in matrix])
+        if matrix[rowIndex][colIndex] == target:
+            return True
+        colIndex -= 1
+    return False
+
+
+matrix = [
+    [1, 4, 7, 11, 15],
+    [2, 5, 8, 12, 19],
+    [3, 6, 9, 16, 22],
+    [10, 13, 14, 17, 24],
+    [18, 21, 23, 26, 30],
+]
+target = 5
+print(searchMatrixII(matrix, target))
