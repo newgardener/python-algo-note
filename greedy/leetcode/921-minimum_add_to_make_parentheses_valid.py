5c641e2911cb4c253bf0ed3dc62239a26ce6f79f
JeongwonShin <jennyshin106@gmail.com>
2024-05-25 23:14:24 +0900

    enhance: get reference from others' solution

--- greedy/leetcode/921-minimum_add_to_make_parentheses_valid.py
+++ greedy/leetcode/921-minimum_add_to_make_parentheses_valid.py
@@ -10,3 +10,18 @@ def minAddToMakeValid(s: str) -> int:
             elif stack[-1] == "(":
                 stack.pop()
     return add + len(stack)
+
+
+def minAddToMakeValid(s: str) -> int:
+    left_balance = 0
+    right_balance = 0
+
+    for ch in s:
+        if ch == "(":
+            left_balance += 1
+        elif ch == ")":
+            if left_balance > 0:
+                left_balance -= 1
+            else:
+                right_balance += 1
+    return left_balance + right_balance
