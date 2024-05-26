c52b3f05359198a547589601815fc12eac76119e
JeongwonShin <jennyshin106@gmail.com>
2024-05-26 17:37:46 +0900

    feat: optimize the answer

--- greedy/leetcode/2091-removing_min_and_max_from_array.py
+++ greedy/leetcode/2091-removing_min_and_max_from_array.py
@@ -2,11 +2,10 @@ from typing import List
 
 
 def minimumDeletions(nums: List[int]) -> int:
-    end = len(nums)
-    minIndex, maxIndex = nums.index(min(nums)), nums.index(max(nums))
-
-    leftSide = max(minIndex + 1, maxIndex + 1)
-    rightSide = max(end - minIndex, end - maxIndex)
-    combined = min(minIndex + 1, end - minIndex) + min(maxIndex + 1, end - maxIndex)
-
-    return min(leftSide, rightSide, combined)
+    n = len(nums)
+    i, j = nums.index(min(nums)), nums.index(max(nums))
+    return min(
+        max(i, j) + 1,
+        n - min(i, j),
+        min(i, j) + 1 + (n - max(i, j)),
+    )
