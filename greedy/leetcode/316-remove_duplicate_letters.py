2fabeebcdf707e16f89df00398889fa8c0b1e09d
JeongwonShin <jennyshin106@gmail.com>
2024-05-26 15:05:13 +0900

    feat: good solution to get a reference from

--- greedy/leetcode/316-remove_duplicate_letters.py
+++ greedy/leetcode/316-remove_duplicate_letters.py
@@ -12,3 +12,20 @@ def removeDuplicateLetters(s: str) -> str:
         count[char] -= 1
 
     return "".join(result)
+
+
+def removeDuplicateLetters(s: str) -> str:
+    stack = []
+    seen = set()
+
+    last_occurrence = {c: i for i, c in enumerate(s)}
+
+    for i, char in enumerate(s):
+        if char not in seen:
+            while stack and char < stack[-1] and i < last_occurrence[stack[-1]]:
+                seen.discard(stack.pop())
+
+            seen.add(char)
+            stack.append(char)
+
+    return "".join(stack)
