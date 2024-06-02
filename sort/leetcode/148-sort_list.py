474831dbd43693940977697e58cba590eb217cc8
JeongwonShin <jennyshin106@gmail.com>
2024-06-02 17:49:22 +0900

    refactor: solution for sort list problem

--- sort/leetcode/148-sort_list.py
+++ sort/leetcode/148-sort_list.py
@@ -8,35 +8,37 @@ class ListNode:
 
 
 def sortList(head: Optional[ListNode]) -> Optional[ListNode]:
-    # base condition
-    if not head or not head.next:
-        return head
+    def sort(head):
+        # base condition
+        if not head or not head.next:
+            return head
 
-    # prev == mid position (subProblem 1)
-    # slow == mid+1 position (subProblem 2)
-    prev = None
-    slow, fast = head, head
-    while fast and fast.next:
-        prev = slow
-        slow = slow.next
-        fast = fast.next.next
+        # prev == mid position (subProblem 1)
+        # slow == mid+1 position (subProblem 2)
+        prev = None
+        slow, fast = head, head
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
 
-    prev.next = None
-    l1 = sortList(head)
-    l2 = sortList(slow)
+        prev.next = None
+        l1 = sort(head)
+        l2 = sort(slow)
+        return merge(l1, l2)
 
-    return merge(l1, l2)
+    def merge(l1, l2):
+        dummy = tail = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
 
+            tail = tail.next
+        tail.next = l1 if l1 else l2
+        return dummy.next
 
-def merge(l1, l2):
-    dummy = tail = ListNode(0)
-    while l1 and l2:
-        if l1.val < l2.val:
-            tail.next = l1
-            l1 = l1.next
-        else:
-            tail.next = l2
-            l2 = l2.next
-        tail = tail.next
-    tail.next = l1 if l1 else l2
-    return dummy.next
+    return sort(head)
