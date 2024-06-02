2c1130da646b7a0e772e43563bbc5a3645e18b3c
JeongwonShin <jennyshin106@gmail.com>
2024-06-02 18:31:40 +0900

    chore: add side note

--- sort/leetcode/932-beautiful_array.py
+++ sort/leetcode/932-beautiful_array.py
@@ -1,5 +1,10 @@
 from typing import List
 
+"""
+Note: odd + even = odd 
+so, segregate the array into two [odd array] + [even array] so that where i < k < j, nums[k] * 2 == nums[i] + nums[j] never happens
+"""
+
 
 def beautifulArray(n: int) -> List[int]:
     if n == 1:
