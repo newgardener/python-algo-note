aaa2a0b5bb2a4800453fa6b8f3a8cf363b70d727
JeongwonShin <jennyshin106@gmail.com>
2024-06-06 17:18:52 +0900

    enhance: solution for median sorted array using binary search

--- sort/leetcode/4-median_of_two_sorted_array.py
+++ sort/leetcode/4-median_of_two_sorted_array.py
@@ -1,5 +1,10 @@
 from typing import List
 
+# %%
+"""
+Time Complexity: O(M+N) where M=len(nums1) and N=len(num2)
+"""
+
 
 def findMedianSortedArrays(nums1: List[int], nums2: List[int]) -> float:
     total = len(nums1) + len(nums2)
@@ -18,3 +23,40 @@ def findMedianSortedArrays(nums1: List[int], nums2: List[int]) -> float:
         k += 1
 
     return result[k] if total % 2 == 1 else (result[k - 1] + result[k]) / 2.0
+
+
+# %%
+"""
+Time Complexity: O(log(M+N)) where M=len(nums1) and N=len(num2)
+ㄴ apply binary search method
+"""
+
+
+def findMedianSortedArray(nums1: List[int], nums2: List[int]) -> float:
+    A, B = nums1, nums2
+    total = len(nums1) + len(nums2)
+    half = total // 2
+
+    if len(B) < len(A):
+        A, B = B, A
+
+    l, r = 0, len(A) - 1
+    while True:
+        i = (l + r) // 2  # A
+        j = half - i - 2  # B
+
+        Aleft = A[i] if i >= 0 else float("-infinity")
+        Aright = A[i + 1] if (i + 1) < len(A) else float("infinity")
+        Bleft = B[j] if j >= 0 else float("-infinity")
+        Bright = B[j + 1] if (j + 1) < len(B) else float("infinity")
+
+        # if partition is correct
+        if Aleft <= Bright and Bleft <= Aright:
+            # odd case
+            if total % 2:
+                return min(Aright, Bright)
+            return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+        elif Aleft > Bright:
+            r = i - 1
+        else:
+            l = i + 1
