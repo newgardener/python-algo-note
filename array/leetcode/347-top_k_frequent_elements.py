d0a85c594bbdc57921cf7e9bc39f13b1e18b5e26
JeongwonShin <jennyshin106@gmail.com>
2024-06-07 00:59:36 +0900

    feat: apply bucket sort method in this problem

--- array/leetcode/347-top_k_frequent_elements.py
+++ array/leetcode/347-top_k_frequent_elements.py
@@ -2,6 +2,10 @@ from typing import List
 from collections import defaultdict
 import heapq
 
+"""
+Time Complexity: O(nlogk) where n=len(nums), k
+"""
+
 
 def topKFrequent(nums: List[int], k: int) -> List[int]:
     numDict = defaultdict(int)
@@ -18,3 +22,29 @@ def topKFrequent(nums: List[int], k: int) -> List[int]:
                 heapq.heappush(arr, (value, key))
 
     return [key for (value, key) in arr]
+
+
+"""
+Bucket Sort
+Time Complexity: O(n)
+[1, 1, 1, 2, 2, 3]
+count(i): [0, 1, 2, 3, 4, 5, 6]
+num: [0, [3], [2], [1], [], [], []]
+"""
+
+
+def topKFrequent(nums: List[int], k: int) -> List[int]:
+    count = {}
+    freq = [[] for i in range(len(nums) + 1)]
+
+    for n in nums:
+        count[n] = count[n].get(n, 0) + 1
+    for n, c in count.items():
+        freq[c].append(n)
+
+    res = []
+    for i in range(len(freq) - 1, 0, -1):
+        for n in freq[i]:
+            res.append(n)
+            if len(res) == k:
+                return res
