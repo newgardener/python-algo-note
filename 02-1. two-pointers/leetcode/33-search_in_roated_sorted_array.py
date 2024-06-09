4bd271310e83970f1f6fe4e1ee03d64cbb6e048a
JeongwonShin <jennyshin106@gmail.com>
2024-06-09 23:34:17 +0900

    feat: enhanced solution not finding pivot index only by binary search

--- 02-1. two-pointers/leetcode/33-search_in_roated_sorted_array.py
+++ 02-1. two-pointers/leetcode/33-search_in_roated_sorted_array.py
@@ -1,6 +1,12 @@
 from typing import List
 
 
+# %%
+"""
+My Solution: do the binary search after finding pivot index
+"""
+
+
 def search(nums: List[int], target: int) -> int:
     last = len(nums) - 1
     pivot = last
@@ -22,4 +28,33 @@ def search(nums: List[int], target: int) -> int:
         else:
             l = mid + 1
 
+    return -1
+
+
+# %%
+"""
+Enhanced Solution: do the binary search w/o finding pivot index
+"""
+
+
+def search(nums: List[int], target: int) -> int:
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        mid = (l + r) // 2
+        if target == nums[mid]:
+            return mid
+
+        # left sorted portion
+        if nums[l] <= nums[mid]:
+            if target > nums[mid] or target < nums[l]:
+                l = mid + 1
+            else:
+                r = mid - 1
+        # right sorted portion
+        else:
+            if target < nums[mid] or target > nums[r]:
+                r = mid - 1
+            else:
+                l = mid + 1
     return -1
