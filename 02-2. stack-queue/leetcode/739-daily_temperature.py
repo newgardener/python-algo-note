42c36ff9ed1e687093a5b235ce00a090336f98c3
JeongwonShin <jennyshin106@gmail.com>
2024-06-15 17:02:51 +0900

    solve: the same problem using monotonic increasing stack

--- 02-2. stack-queue/leetcode/739-daily_temperature.py
+++ 02-2. stack-queue/leetcode/739-daily_temperature.py
@@ -1,7 +1,12 @@
 from typing import List
 
+"""
+Monotonic Decreasing Stack
+Time Complexity: O(N)
+"""
 
-def dailyTemperatures(temperatures: List[int]) -> List[int]:
+
+def dailyTemperaturesMD(temperatures: List[int]) -> List[int]:
     res = [0] * len(temperatures)
     stack = []
 
@@ -9,5 +14,24 @@ def dailyTemperatures(temperatures: List[int]) -> List[int]:
         while stack and t > temperatures[stack[-1]]:
             tempIndex = stack.pop()
             res[tempIndex] = i - tempIndex
+        stack.append(i)
+    return res
+
+
+"""
+Monotonic Increasing Stack
+Time Complexity: O(N)
+"""
+
+
+def dailyTemperaturesMI(temperatures: List[int]) -> List[int]:
+    res = [0] * len(temperatures)
+    stack = []
+
+    for i in range(len(temperatures) - 1, -1, -1):
+        while stack and temperatures[i] >= temperatures[stack[-1]]:
+            stack.pop()
+        if stack:
+            res[i] = stack[-1] - i
         stack.append(i)
     return res
