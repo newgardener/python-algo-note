7f3368fc0a7a57a51eb37815d2a22c616c0e26b6
JeongwonShin <jennyshin106@gmail.com>
2024-06-16 23:49:49 +0900

    feat: optimize time complexity from O(26*N) to O(N)

--- 03-2. sliding-window/leetcode/424-longest_repeating_character_replacement.py
+++ 03-2. sliding-window/leetcode/424-longest_repeating_character_replacement.py
@@ -6,15 +6,19 @@ this leads to time complexity O(26*N)
 
 Further
 how to optimize the time complexity to O(N)?
+=> store mostFrequent in a constant time
+=> the purpose of this problem is to maximize the size of window which means we do not need to update mostFrequent when window size shrinks
 """
 
 
 def characterReplacement(s: str, k: int) -> int:
     i, result = 0, 0
     count = {}
+    mostFrequent = 0
     for j in range(len(s)):
         count[s[j]] = 1 + count.get(s[j], 0)
-        while (j - i + 1) - max(count.values()) > k:
+        mostFrequent = max(mostFrequent, count[s[j]])
+        while (j - i + 1) - mostFrequent > k:
             count[s[i]] -= 1
             i += 1
         result = max(result, j - i + 1)
