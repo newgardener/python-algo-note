77469ae3ce1330da1a1c514f14e2a1eeb6c62506
JeongwonShin <jennyshin106@gmail.com>
2024-06-19 00:27:36 +0900

    solve: linked list cycle ii problem

---
+++ 03-3. linkedlist/leetcode/142-linked_list_cycle_ii.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def detectCycle(head: Optional[ListNode]) -> Optional[ListNode]:
+    slow = fast = head
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
+        if fast == slow:
+            slow = head
+            while slow != fast:
+                fast = fast.next
+                slow = slow.next
+            return slow
+    return None
