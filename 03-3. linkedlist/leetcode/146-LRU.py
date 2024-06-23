7cd631635b03df844ff922dcbe6f29c6f06f6d1e
JeongwonShin <jennyshin106@gmail.com>
2024-06-23 14:25:20 +0900

    solve: LRU problem

--- 03-3. linkedlist/leetcode/146-LRU.py
+++ 03-3. linkedlist/leetcode/146-LRU.py
@@ -1,4 +1,7 @@
-from collections import deque
+class Node:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.prev, self.next = None, None
 
 
 class LRUCache:
@@ -6,27 +9,40 @@ class LRUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.hashmap = {}
-        self.queue = deque([])
+        # left points LRU, right points MRU
+        self.left, self.right = Node(0, 0), Node(0, 0)
+        self.left.next, self.right.prev = self.right, self.left
+
+    # remove a node
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+
+    # insert a node right before right pointer (MRU)
+    def insert(self, node):
+        prev, nxt = self.right.prev, self.right
+        prev.next = nxt.prev = node
+        node.next, node.prev = nxt, prev
 
     def get(self, key: int) -> int:
         if key in self.hashmap:
-            value = self.hashmap[key]
-            while len(self.queue) >= self.capacity:
-                pk, pv = self.queue.popleft()
-            self.queue.append((key, value))
-            return value
+            node = self.hashmap[key]
+            self.remove(node)
+            self.insert(node)
+            return node.val
         return -1
 
     def put(self, key: int, value: int) -> None:
-        while len(self.queue) >= self.capacity:
-            if key in self.hashmap:
-                break
-            pk, pv = self.queue.popleft()
-            if pk in self.hashmap:
-                del self.hashmap[pk]
-        if key not in self.hashmap:
-            self.queue.append((key, value))
-        self.hashmap[key] = value
+        if key in self.hashmap:
+            self.remove(self.hashmap[key])
+        self.hashmap[key] = Node(key, value)
+        self.insert(self.hashmap[key])
+
+        # if above capacity, remove lru
+        if len(self.hashmap) > self.capacity:
+            lru = self.left.next
+            self.remove(lru)
+            del self.hashmap[lru.key]
 
 
 # Your LRUCache object will be instantiated and called as such:
