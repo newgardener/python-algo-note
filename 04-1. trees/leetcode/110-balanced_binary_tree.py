cf803e35f613627662532de22791b03ebe7ce3b8
JeongwonShin <jennyshin106@gmail.com>
2024-06-28 16:14:49 +0900

    solve: balanced binary tree with key takeaways

--- 04-1. trees/leetcode/110-balanced_binary_tree.py
+++ 04-1. trees/leetcode/110-balanced_binary_tree.py
@@ -1,5 +1,11 @@
 from typing import Optional
 
+"""
+Key Points:
+- should check every node that it is balanced
+- should record 1) whether it is balanced so far, 2) maxHeight so far
+"""
+
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -10,17 +16,20 @@ class TreeNode:
 
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        if not root:
-            return True
+        # (isBalanced, maxHeight)
+        def checkLRHeight(node):
+            if not node:
+                return (True, 0)
+            leftBalanced, leftHeight = self.checkLRHeight(node.left)
+            rightBalanced, rightHeight = self.checkLRHeight(node.right)
+            maxHeight = max(leftHeight, rightHeight) + 1
+            if (
+                not leftBalanced
+                or not rightBalanced
+                or abs(leftHeight - rightHeight) > 1
+            ):
+                return (False, maxHeight)
+            return (True, maxHeight)
 
-        left = self.getHeight(root.left)
-        right = self.getHeight(root.right)
-
-        return abs(left - right) <= 1
-
-    def getHeight(self, node):
-        if not node:
-            return 0
-        left = self.getHeight(node.left) + 1
-        right = self.getHeight(node.right) + 1
-        return max(left, right)
+        balanced, _ = checkLRHeight(root)
+        return balanced
