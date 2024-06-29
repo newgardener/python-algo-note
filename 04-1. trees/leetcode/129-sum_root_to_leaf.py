db5916d4053a3a4e769d4acdf9dac7b738d44f07
JeongwonShin <jennyshin106@gmail.com>
2024-06-29 15:00:32 +0900

    review: solve sum root to leaf using bfs

--- 04-1. trees/leetcode/129-sum_root_to_leaf.py
+++ 04-1. trees/leetcode/129-sum_root_to_leaf.py
@@ -21,38 +21,19 @@ def sumNumbers(root: Optional[TreeNode]) -> int:
     def isLeafNode(node):
         return node and not node.left and not node.right
 
-    def restorePath(leaf, parents):
-        path = []
-        while leaf != root:
-            path.append(str(leaf.val))
-            leaf = parents[leaf]
-        path.append(str(root.val))
-        return "".join(path[::-1])
-
-    def bfs(start):
-        parents = {}
-        leafNodes = []
-        queue = deque([start])
-
-        while queue:
-            node = queue.popleft()
-            if isLeafNode(node):
-                leafNodes.append(node)
-                continue
-            if node.left:
-                parents[node.left] = node
-                queue.append(node.left)
-            if node.right:
-                parents[node.right] = node
-                queue.append(node.right)
-        return leafNodes, parents
-
-    leafNodes, parents = bfs(root)
-
-    result = 0
-    for leaf in leafNodes:
-        result += int(restorePath(leaf, parents))
-    return result
+    # (node, path)
+    q = deque([(root, str(root.val))])
+    result = []
+    while q:
+        node, path = q.popleft()
+        if isLeafNode(node):
+            result.append(int(path))
+            continue
+        if node.left:
+            q.append((node.left, path + str(node.left.val)))
+        if node.right:
+            q.append((node.right, path + str(node.right.val)))
+    return sum(result)
 
 
 # %%
