2669d8709a6c9fba34a2244ee8d619d6ac32a83f
JeongwonShin <jennyshin106@gmail.com>
2024-06-29 16:03:36 +0900

    add: another solution using queue (FIFO)

--- 04-1. trees/leetcode/102-binary_tree_level_order_traversal.py
+++ 04-1. trees/leetcode/102-binary_tree_level_order_traversal.py
@@ -10,7 +10,11 @@ class TreeNode:
 
 
 #%%
-# dfs
+"""
+DFS solution
+"""
+
+
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         cache = collections.defaultdict(list)
@@ -24,3 +28,33 @@ class Solution:
 
         dfs(root)
         return [value for value in cache.values()]
+
+
+#%%
+"""
+BFS solution
+Key point:
+- use a queue to keep track of nodes in the same level
+- FIFO
+"""
+
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        result = []
+        q = collections.deque([root])
+        while q:
+            # nodes in the same level are inserted
+            level = []
+            for _ in range(len(q)):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    if node.left:
+                        q.append(node.left)
+                    if node.right:
+                        q.append(node.right)
+            if level:
+                result.append(level)
+        return result
+
