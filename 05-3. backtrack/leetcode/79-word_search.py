dd356665634d8d915b2c5df4b604a50b59b655b4
JeongwonShin <jennyshin106@gmail.com>
2024-07-07 14:49:54 +0900

    enhance: word search solution using word index

--- 05-3. backtrack/leetcode/79-word_search.py
+++ 05-3. backtrack/leetcode/79-word_search.py
@@ -1,5 +1,11 @@
 from typing import List
 
+# %%
+"""
+Problem: we don't need to hold all characters in the path  
+Solution: increment word index by 1 and if word index reaches the word length, it means we found it!
+"""
+
 
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
@@ -36,4 +42,38 @@ class Solution:
                 if board[i][j] == word[0]:
                     if dfs((i, j), [word[0]]):
                         return True
+        return False
+
+
+# %%
+
+"""
+l = length of word, 4 directions (up, down, left, right)
+Time Complexity: (m * n * 4^l)
+"""
+
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        m, n = len(board), len(board[0])
+        visited = set()
+
+        def isBounded(r, c):
+            return 0 <= r < m and 0 <= c < n
+
+        def dfs(r, c, idx):
+            if idx == len(word):
+                return True
+            if not isBounded(r, c) or (r, c) in visited or board[r][c] != word[idx]:
+                return False
+
+            visited.add((r, c))
+            res = dfs(r, c + 1, idx + 1) or dfs(r, c - 1, idx + 1) or dfs(r + 1, c, idx + 1) or dfs(r - 1, c, idx + 1)
+            visited.remove((r, c))
+            return res
+
+        for r in range(m):
+            for c in range(n):
+                if dfs(r, c, 0):
+                    return True
         return False
