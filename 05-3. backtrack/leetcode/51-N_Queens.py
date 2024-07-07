5503ce8d18253cedc495210084f94c6ae5cfd157
JeongwonShin <jennyshin106@gmail.com>
2024-07-07 23:09:45 +0900

    enhance: time complexity of checking validity of placing queen

--- 05-3. backtrack/leetcode/51-N_Queens.py
+++ 05-3. backtrack/leetcode/51-N_Queens.py
@@ -1,5 +1,15 @@
 from typing import List
 
+"""
+Time Complexity:
+- isValid: O(N) + O(N) + O(N) = O(N)
+- dfs: O(N^n) 
+=> O(N^n * N) = (N^(n+1))
+"""
+
+
+# %%
+
 
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
@@ -39,6 +49,52 @@ class Solution:
                     continue
                 board[row][col] = "Q"
                 dfs(row + 1, board)
+                board[row][col] = "."
+
+        dfs(0, board)
+        return result
+
+
+# %%
+"""
+Time Complexity:
+- isValid: O(1)
+- dfs: O(N^n)
+=> O(N^n)
+"""
+
+
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        result = []
+        board = [["."] * n for _ in range(n)]
+
+        vertical = set()
+        rightDiagonal = set()
+        leftDiagonal = set()
+
+        def isValid(row, col):
+            return col not in vertical and (row + col) not in rightDiagonal and (row - col) not in leftDiagonal
+
+        def dfs(row, board):
+            if row >= n:
+                solution = []
+                for i in range(n):
+                    solution.append("".join(board[i]))
+                result.append(solution)
+                return
+            for col in range(n):
+                if not isValid(row, col):
+                    continue
+
+                board[row][col] = "Q"
+                vertical.add(col)
+                rightDiagonal.add(row + col)
+                leftDiagonal.add(row - col)
+                dfs(row + 1, board)
+                vertical.remove(col)
+                rightDiagonal.remove(row + col)
+                leftDiagonal.remove(row - col)
                 board[row][col] = "."
 
         dfs(0, board)
