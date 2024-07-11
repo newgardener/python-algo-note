dcba1c0be54ac9e43efc3836693faece57b0322e
JeongwonShin <jennyshin106@gmail.com>
2024-07-11 22:39:29 +0900

    solve: count complete tree nodes problem

---
+++ 04. trees/leetcode/222-count_complete_tree_nodes.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        l, r = root, root
+        lh, rh = 0, 0
+        # calculate height of a left subtree
+        while l:
+            l = l.left
+            lh += 1
+        # calculate height of a right subtree
+        while r:
+            r = r.right
+            rh += 1
+
+        # perfect binary tree
+        if lh == rh:
+            return 2 ** lh - 1
+        # complete binary tree
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
