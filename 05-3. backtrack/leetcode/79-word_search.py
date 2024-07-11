542a425bf9e221558eed7b28ce40c2faf8ae5ce3
JeongwonShin <jennyshin106@gmail.com>
2024-07-12 00:56:29 +0900

    delete: inefficient approach of recursion

--- 05-3. backtrack/leetcode/79-word_search.py
+++ 05-3. backtrack/leetcode/79-word_search.py
@@ -1,52 +1,5 @@
 from typing import List
 
-# %%
-"""
-Problem: we don't need to hold all characters in the path  
-Solution: increment word index by 1 and if word index reaches the word length, it means we found it!
-"""
-
-
-class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        m, n = len(board), len(board[0])
-        dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        visited = set()
-
-        def isBounded(x, y):
-            return 0 <= x < m and 0 <= y < n
-
-        def dfs(pos, path):
-            i, j = pos
-            pathWord = ''.join(path)
-
-            if pathWord != word[:len(path)]:
-                return False
-
-            if pathWord == word:
-                return True
-
-            visited.add((i, j))
-
-            for dx, dy in dirs:
-                newPos = (i + dx, j + dy)
-                if isBounded(*newPos) and newPos not in visited:
-                    if dfs(newPos, path + [board[dx][dy]]):
-                        return True
-
-            visited.remove((i, j))
-            return False
-
-        for i in range(m):
-            for j in range(n):
-                if board[i][j] == word[0]:
-                    if dfs((i, j), [word[0]]):
-                        return True
-        return False
-
-
-# %%
-
 """
 l = length of word, 4 directions (up, down, left, right)
 Time Complexity: (m * n * 4^l)
