95d514cde31e06ec2b678ad7b134cd79a485cab4
JeongwonShin <jennyshin106@gmail.com>
2024-07-12 00:47:43 +0900

    add: take-away recursion insights from this problem

--- 05-1. trie/leetcode/211-design_add_search_words.py
+++ 05-1. trie/leetcode/211-design_add_search_words.py
@@ -1,3 +1,15 @@
+"""
+Take-Away:
+Q. passing the remaining part of the word or passing the current index, which is better?
+   search(node, word[1:]) or search(node, i+1)
+A. In terms of Space Complexity,
+M = length of the word
+- passing the remaining part: O(M^2)
+- passing the index: O(M)
+When creating a recursion, index-based approach is better in terms of memory efficiency
+"""
+
+
 class TrieNode:
     def __init__(self):
         self.children = {}
@@ -33,31 +45,33 @@ class WordDictionary:
     def search(self, word: str) -> bool:
         return self._search(self.root, word)
 
-    # %%
 
-    class WordDictionary:
-        def __init__(self):
-            self.root = TrieNode()
+# %%
 
-        def addWord(self, word: str) -> None:
-            node = self.root
-            for ch in word:
+
+class WordDictionary:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        node = self.root
+        for ch in word:
+            if ch not in node.children:
+                node.children[ch] = TrieNode()
+            node = node.children[ch]
+        node.isEnd = True
+
+    def search(self, word: str) -> bool:
+        def dfs(node, i):
+            ch = word[i]
+            if ch == ".":
+                for child in node.children.values():
+                    if dfs(child, i + 1):
+                        return True
+                return False
+            else:
                 if ch not in node.children:
-                    node.children[ch] = TrieNode()
-                node = node.children[ch]
-            node.isEnd = True
-
-        def search(self, word: str) -> bool:
-            def dfs(node, i):
-                ch = word[i]
-                if ch == ".":
-                    for child in node.children.values():
-                        if dfs(child, i + 1):
-                            return True
                     return False
-                else:
-                    if ch not in node.children:
-                        return False
-                    return dfs(node.children[ch], i + 1)
+                return dfs(node.children[ch], i + 1)
 
-            return dfs(self.root, 0)
+        return dfs(self.root, 0)
