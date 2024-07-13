60a9cef0e815106c910dba684a25bd3783650672
JeongwonShin <jennyshin106@gmail.com>
2024-07-13 22:54:37 +0900

    solve: rotting oranges problem

--- 06-1. graphs/leetcode/994-rotting_oranges.py
+++ 06-1. graphs/leetcode/994-rotting_oranges.py
@@ -1,37 +1,36 @@
+import collections
 from typing import List
+
+"""
+Take-Away:
+problems involving simultaneous propagation or infection across a grid, "BFS" is usually more suitable than DFS
+BFS = level-by-level traversal
+"""
 
 
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        fresh = set()
-        rotten = set()
+        time = 0
+        fresh = 0
+        q = collections.deque()
+
         for i in range(m):
             for j in range(n):
-                # fresh orange
-                if grid[i][j] == 1:
-                    fresh.add((i, j))
+                # store rotten oranges
                 if grid[i][j] == 2:
-                    rotten.add((i, j))
+                    q.append((i, j))
+                # count fresh oranges
+                if grid[i][j] == 1:
+                    fresh += 1
 
-        def dfs(i, j, time):
-            if (i, j) not in fresh:
-                return time
-
-            # make an orange rotten
-            fresh.remove((i, j))
-            return max(
-                time,
-                dfs(i - 1, j, time + 1),
-                dfs(i + 1, j, time + 1),
-                dfs(i, j - 1, time + 1),
-                dfs(i, j + 1, time + 1)
-            )
-
-        maxMin = 0
-        dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        for i, j in rotten:
-            for dx, dy in dirs:
-                if (i + dx, j + dy) in fresh:
-                    maxMin = max(maxMin, dfs(i + dx, j + dy, 1))
-        return maxMin if not fresh else -1
+        while q and fresh >= 0:
+            x, y, time = q.popleft()
+            for nx, ny in [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]:
+                # level-by-level propagation
+                if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] == 1:
+                    # update status (grid state, fresh count)
+                    grid[nx][ny] = 2
+                    fresh -= 1
+                    q.append((nx, ny))
+        return time if fresh == 0 else -1
