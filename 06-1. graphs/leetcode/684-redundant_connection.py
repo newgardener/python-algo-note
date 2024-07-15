9b832440372b33b7418110ca3d3b5306bdfef42b
JeongwonShin <jennyshin106@gmail.com>
2024-07-15 23:34:31 +0900

    solve: redundant cycle problem using union find

--- 06-1. graphs/leetcode/684-redundant_connection.py
+++ 06-1. graphs/leetcode/684-redundant_connection.py
@@ -3,6 +3,8 @@ from typing import List
 # %%
 """
 use DFS to find a cycle in undirected graph
+Time Complexity: O(n^2)
+- dfs runs on n number of edges and each dfs takes O(n) of time 
 """
 
 
@@ -27,3 +29,41 @@ class Solution:
             # undirected graph
             graph[u].append(v)
             graph[v].append(u)
+
+
+# %%
+class UnionFind:
+    def __init__(self, size):
+        self.parent = list(range(size))
+        self.rank = [0] * size
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        px, py = self.find(x), self.find(y)
+        # if we add this edge, we make a cycle
+        if px == py:
+            return False
+        # union edges
+        if self.rank[px] > self.rank[py]:
+            self.parent[py] = px
+        elif self.rank[px] < self.rank[py]:
+            self.parent[px] = py
+        else:
+            self.parent[py] = px
+            self.rank[px] += 1
+        return True
+
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        uf = UnionFind(len(edges) + 1)
+
+        for u, v in edges:
+            if not uf.union(u, v):
+                return [u, v]
+
+        return []
