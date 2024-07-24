96bafe3d26a6d8ad481ee366fe5b169495f5e3fe
JeongwonShin <jennyshin106@gmail.com>
2024-07-24 21:20:27 +0900

    solve: min cost to connect all points problem

--- 06-1. graphs/leetcode/advanced/1584-min_cost_to_connect_all_points.py
+++ 06-1. graphs/leetcode/advanced/1584-min_cost_to_connect_all_points.py
@@ -5,19 +5,46 @@ from typing import List
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         n = len(points)
-        points.sort()
-        pq = [(0, points[0])]
-        distances = [float('infinity')] * n
-        distances[0] = 0
+        # create edges (pointX, pointY, manhattan distance)
+        edges = []
+        for i in range(n):
+            for j in range(i + 1, n):
+                x1, y1 = points[i]
+                x2, y2 = points[j]
+                dist = abs(x1 - x2) + abs(y1 - y2)
+                edges.append((i, j, dist))
 
-        while pq:
-            index, pos = heapq.heappop(pq)
-            x1, y1 = pos
-            for i in range(index + 1, n):
-                x2, y2 = points[i]
-                md = abs(x1 - x2) + abs(y1 - y2)
-                if md < distances[i]:
-                    distances[i] = md
-                    heapq.heappush(pq, (i, points[i]))
+        parents = list(range(n))
+        rank = [0] * n
 
-        return sum(distances)
+        def find(x):
+            if x != parents[x]:
+                parents[x] = find(parents[x])
+            return parents[x]
+
+        def union(x, y):
+            px, py = find(x), find(y)
+            # already a union
+            if px == py:
+                return False
+            # make them into a union
+            if rank[px] > rank[py]:
+                parents[py] = px
+                rank[px] += rank[py]
+            else:
+                parents[px] = py
+                rank[py] += rank[px]
+            return True
+
+        # sort edges in an ascending distance order
+        edges.sort(key=lambda x: x[2])
+        minCost = 0
+        edgeCnt = 0
+        for p1, p2, dist in edges:
+            if union(p1, p2):
+                minCost += dist
+                edgeCnt += 1
+                if edgeCnt == n - 1:
+                    break
+
+        return minCost
