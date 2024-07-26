93d1ebec087ad6cda47ecc02530f555b51938c8b
JeongwonShin <jennyshin106@gmail.com>
2024-07-26 15:42:32 +0900

    feat: dfs + backtrack solution for reconstruct itinerary problem

---
+++ 06-1. graphs/leetcode/advanced/332-reconstruct_itinerary.py
@@ -0,0 +1,41 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        n = len(tickets)
+        graph = defaultdict(list)
+        ticketCnt = defaultdict(lambda: defaultdict(int))
+        for frm, to in tickets:
+            graph[frm].append(to)
+            ticketCnt[frm][to] += 1
+
+        for frm in graph:
+            graph[frm].sort()  # sort in ascending order
+
+        result = ["JFK"]
+
+        def dfs(node):
+            if len(result) == n + 1:
+                return True
+
+            if node not in graph:
+                return False
+
+            for nextNode in graph[node]:
+                if ticketCnt[node][nextNode] > 0:
+                    ticketCnt[node][nextNode] -= 1
+                    result.append(nextNode)
+
+                    if dfs(nextNode):
+                        return True
+
+                    # backtrack
+                    ticketCnt[node][nextNode] += 1
+                    result.pop()
+
+            return False
+
+        dfs("JFK")
+        return result
