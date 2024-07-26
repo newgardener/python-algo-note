b8311f3c540ad576fe8239665844cef5ebeb5358
JeongwonShin <jennyshin106@gmail.com>
2024-07-26 18:01:36 +0900

    add: time complexity analysis note to task scheduler

--- 05-2. heap/leetcode/621-task_schedular.py
+++ 05-2. heap/leetcode/621-task_schedular.py
@@ -2,6 +2,16 @@ from collections import Counter, deque
 import heapq
 from typing import List
 
+"""
+Time Complexity:
+K = # of unique tasks, N = # of tasks
+- initial heap creation: O(K)
+- while loop: O(NlogK), but in worst case O(NlogN)
+
+=> best/average case: O(N) when K is small and constant
+=> worst case: O(NlogN)
+"""
+
 
 # each task 1 unit time
 # purpose is to minimize idle time
@@ -16,10 +26,13 @@ def leastInterval(tasks: List[str], n: int) -> int:
     while maxHeap or q:
         time += 1
         if maxHeap:
+            # execute task
             cnt = 1 + heapq.heappop(maxHeap)
+            # schedule next task
             if cnt:
                 q.append([cnt, time + n])
 
+        # if cooling time is met, push to maxHeap
         if q and q[0][1] == time:
             heapq.heappush(maxHeap, q.popleft()[0])
 
