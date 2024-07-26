da38470f22da18067f9d235291df099d2be9568f
JeongwonShin <jennyshin106@gmail.com>
2024-07-26 15:59:18 +0900

    solve: reconstruct itinerary problem applying Hierholzer's Algorithm

--- 06-1. graphs/leetcode/advanced/332-reconstruct_itinerary.py
+++ 06-1. graphs/leetcode/advanced/332-reconstruct_itinerary.py
@@ -7,7 +7,6 @@ DFS + Backtracking Solution => but hit TLE why?
 N = # of tickets, K = max # of dst from src
 Time Complexity: O(N^K) at worst it can explore all possible paths
 Space Complexity: O(N)
-
 """
 
 
@@ -48,3 +47,35 @@ class Solution:
 
         dfs("JFK")
         return result
+
+
+# %%
+"""
+Hierholzer's Algorithm
+Time Complexity:
+- main algorithm part (while loop) is O(N) => each edge is processed only once
+=> overall time complexity is O(NlogK) for sorting part
+Space Complexity: O(N)
+
+=> Hierholzer's Algorithm excels in time complexity compared to DFS + Backtracking Algorithm 
+"""
+
+
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        n = len(tickets)
+        graph = defaultdict(list)
+        for frm, to in tickets:
+            graph[frm].append(to)
+
+        for frm in graph:
+            graph[frm].sort(reverse=True)  # in order to get lexically smaller dst when popping
+
+        stack = ["JFK"]
+        res = []
+        while stack:
+            while graph[stack[-1]]:
+                stack.append(graph[stack[-1]].pop())
+            else:
+                res.append(stack.pop())
+        return res[::-1]
