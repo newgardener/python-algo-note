96586a5f2d275f0214170bf7eded353bc81f917f
JeongwonShin <jennyshin106@gmail.com>
2024-07-28 14:07:07 +0900

    solve: sliding window solution for getting maxL, maxR index of maximum subarray

--- 08. greedy/leetcode/53-maximum_subarray.py
+++ 08. greedy/leetcode/53-maximum_subarray.py
@@ -44,5 +44,20 @@ Sliding Window to get L, R index of a maximum subarray
 
 
 class Solution:
-    def slidingwindow(self, nums: List[int]) -> int:
-        pass
+    def slidingwindow(self, nums: List[int]) -> list[int]:
+        maxSum = nums[0]
+        curSum = 0
+        L = 0
+        maxL, maxR = 0, 0
+
+        for R in range(len(nums)):
+            if curSum < 0:
+                curSum = 0
+                L = R
+
+            curSum += nums[R]
+            if curSum > maxSum:
+                maxSum = curSum
+                maxL, maxR = L, R
+
+        return [maxL, maxR]
