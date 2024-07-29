345d905978b402e5a9a2293813209d04eeed5f81
JeongwonShin <jennyshin106@gmail.com>
2024-07-29 22:33:51 +0900

    add: take-away and time complexity notes

--- 08. greedy/leetcode/134-gas_station.py
+++ 08. greedy/leetcode/134-gas_station.py
@@ -1,5 +1,14 @@
 from typing import List
 
+"""
+Take-Away:
+- early return an impossible case
+- for each step, record a surplus and if we cannot make it at point i, move onto i+1 
+
+greedy approach can make a big time complexity enhancement O(N^2) -> O(N)
+Time Complexity: O(N)
+"""
+
 
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
