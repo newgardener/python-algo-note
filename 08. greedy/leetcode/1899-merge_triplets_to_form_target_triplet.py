a73284ac4cbb78799aa32cddf237f406826c68e7
JeongwonShin <jennyshin106@gmail.com>
2024-07-31 23:42:28 +0900

    chore: add take-aways and time complexity notes

--- 08. greedy/leetcode/1899-merge_triplets_to_form_target_triplet.py
+++ 08. greedy/leetcode/1899-merge_triplets_to_form_target_triplet.py
@@ -1,5 +1,13 @@
 from typing import List
 
+"""
+Take-Away:
+- remove redundant items first (in this case, an item which holds greater number that target)
+- early-return if condition is met (in this case, found all three target numbers)
+
+Time Complexity: O(N)
+"""
+
 
 def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
     filtered = [item for item in triplets if item[0] <= target[0] and item[1] <= target[1] and item[2] <= target[2]]
