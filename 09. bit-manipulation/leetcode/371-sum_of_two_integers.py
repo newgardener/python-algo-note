5e3336029cb5410dd65e57b86dca60253d69ccd7
JeongwonShin <jennyshin106@gmail.com>
2024-08-01 23:58:05 +0900

    add: notes for why we should use mask in python

--- 09. bit-manipulation/leetcode/371-sum_of_two_integers.py
+++ 09. bit-manipulation/leetcode/371-sum_of_two_integers.py
@@ -1,3 +1,11 @@
+"""
+Why we should use mask?
+- in order to represent 32-bit integer representation
+ㄴ Python uses arbitrary-precision integers, which can grow to use as much memory as necessary to represent the integer
+ㄴ 32-bit mask helps us simulate 32-bit integer
+"""
+
+
 class Solution:
     def getSum(self, a: int, b: int) -> int:
         # to retrieve only 32-bit value part
@@ -9,4 +17,3 @@ class Solution:
             b = carry
 
         return a & mask if b > 0 else a
-
