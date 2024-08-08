22d72d6e916c4902d54e8c7de80d478366abde03
JeongwonShin <jennyshin106@gmail.com>
2024-08-08 23:39:30 +0900

    solve: coin change problem again with template

--- 06-2. dp/leetcode/322-coin_change.py
+++ 06-2. dp/leetcode/322-coin_change.py
@@ -1,6 +1,5 @@
 from typing import List
 
-
 """
 Unbounded Knapsack Problem
 Time Complexity: O(N*M) where N=len(coins), M=amount
@@ -11,9 +10,9 @@ def coinChange(coins: List[int], amount: int) -> int:
     dp = [float("inf")] * (amount + 1)
     dp[0] = 0
 
-    for i in range(1, amount + 1):
-        for c in coins:
-            if c <= i:
-                dp[i] = min(dp[i], 1 + dp[i - c])
+    for i in range(len(coins)):
+        for w in range(1, amount + 1):
+            if w >= i:
+                dp[w] = min(dp[w], 1 + dp[w - coins[i]])
 
     return dp[amount] if dp[amount] != float("inf") else -1
