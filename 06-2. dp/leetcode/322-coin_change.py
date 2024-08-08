7c61f2cb95c8861628340adc5063a9482a37a314
JeongwonShin <jennyshin106@gmail.com>
2024-08-08 23:43:08 +0900

    add: unbounded knapsack template description

--- 06-2. dp/leetcode/322-coin_change.py
+++ 06-2. dp/leetcode/322-coin_change.py
@@ -10,6 +10,7 @@ def coinChange(coins: List[int], amount: int) -> int:
     dp = [float("inf")] * (amount + 1)
     dp[0] = 0
 
+    # i개의 coin 중 가장 적은 수의 coin을 사용해서 금액 w를 만들 수 있는 방법
     for i in range(len(coins)):
         for w in range(1, amount + 1):
             if w >= i:
