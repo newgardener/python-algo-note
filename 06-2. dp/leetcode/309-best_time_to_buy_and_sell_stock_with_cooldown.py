f09eb25fc94474f14d0fb458d3273dc6434c0608
JeongwonShin <jennyshin106@gmail.com>
2024-08-10 23:58:30 +0900

    solve: best time to buy and sell stock with cooldown with DP

---
+++ 06-2. dp/leetcode/309-best_time_to_buy_and_sell_stock_with_cooldown.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        dp = {}
+
+        def dfs(i, buying):
+            if i >= n:
+                return 0
+            if (i, buying) in dp:
+                return dp[(i, buying)]
+
+            cooldown = dfs(i + 1, buying)
+            if buying:
+                buy = dfs(i + 1, not buying) - prices[i]
+                dp[(i, buying)] = max(buy, cooldown)
+            else:
+                sell = dfs(i + 2, not buying) + prices[i]
+                dp[(i, buying)] = max(sell, cooldown)
+            return dp[(i, buying)]
+
+        return dfs(0, True)
