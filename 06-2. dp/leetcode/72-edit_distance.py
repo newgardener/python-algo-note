5f1f8d05a341bc966bbee93b606c253ec7b3d904
JeongwonShin <jennyshin106@gmail.com>
2024-08-11 14:48:50 +0900

    solve: edit distance using dp

---
+++ 06-2. dp/leetcode/72-edit_distance.py
@@ -0,0 +1,24 @@
+# %%
+# 2D DP Solution
+
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        m, n = len(word1), len(word2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        # base case (from empty string to target character)
+        for i in range(1, m + 1):
+            dp[i][0] = i
+        for j in range(1, n + 1):
+            dp[0][j] = j
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                # matched
+                if word1[i - 1] == word2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                else:
+                    # replace: dp[i-1][j-1]
+                    # insert: dp[i][j-1]
+                    # delete: dp[i-1][j]
+                    dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1
+        return dp[m][n]
