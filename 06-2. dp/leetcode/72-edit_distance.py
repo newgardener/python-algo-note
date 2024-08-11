4ca699bd577395aa20a00843981d6ee2e9f18855
JeongwonShin <jennyshin106@gmail.com>
2024-08-11 14:57:07 +0900

    solve: edit distance using recursion + memoization

--- 06-2. dp/leetcode/72-edit_distance.py
+++ 06-2. dp/leetcode/72-edit_distance.py
@@ -11,6 +11,7 @@ class Solution:
         for j in range(1, n + 1):
             dp[0][j] = j
 
+        # dp (= bottom-up approach)
         for i in range(1, m + 1):
             for j in range(1, n + 1):
                 # matched
@@ -22,3 +23,30 @@ class Solution:
                     # delete: dp[i-1][j]
                     dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1
         return dp[m][n]
+
+
+# %%
+
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        memo = {}
+
+        def dp(i, j):
+            # caching
+            if (i, j) in memo:
+                return memo[(i, j)]
+
+            # base case
+            if i == -1:
+                return j + 1
+            if j == -1:
+                return i + 1
+
+            # recursive (= top-down approach)
+            if word1[i] == word2[j]:
+                memo[(i, j)] = dp(i - 1, j - 1)
+            else:
+                memo[(i, j)] = min(dp(i - 1, j - 1), dp(i - 1, j), dp(i, j - 1)) + 1
+            return memo[(i, j)]
+
+        return dp(len(word1) - 1, len(word2) - 1)
