ad2079a4e884f1ae624e0cf067dc6a6f49bd9f8a
JeongwonShin <jennyshin106@gmail.com>
2024-08-13 00:54:05 +0900

    solve: target sum problem using 0-1 knapsack (2D)

--- 06-2. dp/leetcode/knapsack(0-1)/494-target_sum.py
+++ 06-2. dp/leetcode/knapsack(0-1)/494-target_sum.py
@@ -5,6 +5,9 @@ Time Complexity: O(N * T) where N=len(nums) and T=sum(nums)
 """
 
 
+# %%
+# DFS + Memoization
+
 def findTargetSumWays(nums: List[int], target: int) -> int:
     dp = {}
 
@@ -19,3 +22,44 @@ def findTargetSumWays(nums: List[int], target: int) -> int:
         return dp[(i, total)]
 
     return dfs(0, 0)
+
+
+# %%
+# 2D (knapsack 0-1)
+
+"""
+sum(A): + set, sum(B): - set
+sum(A) - sum(B) = target
+sum(A) + sum(A) = target + sum(B) + sum(A)
+sum(A) * 2 = target + sum(nums)
+=> sum(A) = (target + sum(nums)) // 2
+
+Define Problem with 0-1 knapsack template
+dp[i][j] = 물건(=숫자) i개로 배낭 용량(=target) j를 채울 수 있는 방법 수 단, 각 물건은 한 번씩만 사용 가능 (=bounded knapsack)  
+dp[n][numToReach]
+"""
+
+
+def findTargetSumWays(nums: List[int], target: int) -> int:
+    total = sum(nums)
+    n = len(nums)
+    if total < abs(target) or (total + target) % 2 == 1:
+        return 0
+    numToReach = (total + target) // 2
+
+    dp = [[0] * (numToReach + 1) for _ in range(n + 1)]
+    # base case
+    # 물건 i개로 용량 0을 채울 수 있는 방법은 1가지 => 배낭에 넣지 않기
+    for i in range(n + 1):
+        dp[i][0] = 1
+    for i in range(1, n + 1):
+        for j in range(numToReach + 1):
+            # 용량이 충분할 때
+            # dp[i-1][j]: 배낭에 넣지 않기
+            # dp[i-1][j-nums[i-1]]: 배낭에 넣기
+            if j >= nums[i - 1]:
+                dp[i][j] = dp[i - 1][j] + dp[i - 1][j - nums[i - 1]]
+            # 용량이 부족할 때
+            else:
+                dp[i][j] = dp[i - 1][j]
+    return dp[n][numToReach]
