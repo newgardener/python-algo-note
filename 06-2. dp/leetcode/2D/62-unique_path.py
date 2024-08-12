7d28e39e4ab98bd50ebb6bd2c3ae500778e131e0
JeongwonShin <jennyshin106@gmail.com>
2024-08-13 00:34:55 +0900

    solve: unique path problem both 1D, 2D ways

--- 06-2. dp/leetcode/2D/62-unique_path.py
+++ 06-2. dp/leetcode/2D/62-unique_path.py
@@ -1,7 +1,34 @@
-class Solution:
-    def uniquePaths(self, m: int, n: int) -> int:
-        dp = [1] * n
-        for i in range(1, m):
-            for j in range(1, n):
-                dp[j] += dp[j - 1]
-        return dp[n - 1]
+"""
+Time Complexity: O(m*n)
+"""
+
+
+# %%
+# 2D DP
+# Space Complexity: O(m*n)
+
+def uniquePaths(self, m: int, n: int) -> int:
+    dp = [[0] * n for _ in range(m)]
+    for i in range(n):
+        dp[0][i] = 1
+    for j in range(m):
+        dp[j][0] = 1
+
+    for i in range(1, m):
+        for j in range(1, n):
+            dp[i][j] = dp[i][j - 1] + dp[i - 1][j]
+    return dp[m - 1][n - 1]
+
+
+# %%
+# 1D DP
+# Space Complexity: O(n)
+
+def uniquePaths(self, m: int, n: int) -> int:
+    dp = [1] * n
+    for i in range(1, m):
+        for j in range(1, n):
+            # dp[j]: cell from a prev row (top)
+            # dp[j-1]: cell from a current row (left)
+            dp[j] += dp[j - 1]
+    return dp[n - 1]
