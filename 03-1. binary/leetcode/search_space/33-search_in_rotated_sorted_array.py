e2a8c5198792253cc5e06de2e3359e38c7a214aa
JeongwonShin <jennyshin106@gmail.com>
2024-08-22 22:00:14 +0900

    solve: search in rotated sorted array

---
+++ 03-1. binary/leetcode/search_space/33-search_in_rotated_sorted_array.py
@@ -0,0 +1,29 @@
+from typing import List
+
+"""
+number array (num) does not allow duplicates
+=> by comparing nums[mid] and nums[l], we can know whether we are in left sorted portion or right sorted portion
+"""
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = l + (r - l) // 2
+            if nums[mid] == target:
+                return mid
+            # left sorted portion
+            if nums[l] <= nums[mid]:
+                if target > nums[mid] or target < nums[l]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            # right sorted portion
+            else:
+                if target < nums[mid] or target > nums[r]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+        # not found
+        return -1
