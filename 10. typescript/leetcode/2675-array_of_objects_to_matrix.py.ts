d6ab85c5ff33e991049455e8e010926508cb9d1f
newgardener <jennyshin106@gmail.com>
2024-09-02 00:23:57 +0900

    solve: array of objects to matrix problem

--- 10. typescript/leetcode/2675-array_of_objects_to_matrix.py.ts
+++ 10. typescript/leetcode/2675-array_of_objects_to_matrix.py.ts
@@ -1,53 +1,66 @@
-type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type primitive = null | boolean | number | string;
+type JSONValue =
+  | null
+  | boolean
+  | number
+  | string
+  | JSONValue[]
+  | { [key: string]: JSONValue };
 
-function jsonToMatrix(arr: JSONValue[]): (null | boolean | number | string)[][] {
-    const keyValMap = new Map()
-    const matrix = Array.from({ length: arr.length }, () => new Array());
-    const rows = Array.from({ length: arr.length }, () => new Array());
+function jsonToMatrix(
+  arr: JSONValue[]
+): (null | boolean | number | string)[][] {
+  const keys = Array.from(new Set(arr.flatMap((obj) => getKeys(obj)))).sort();
 
-    arr.forEach((item, index) => {
-        for (const [key, value] of Object.entries(item)) {
-            // empty obj
-            if (!key && !value) {
-                rows[index].push([])
-            }
-            if (value !== null && typeof value === 'object') {
-                // nested obj
-                const keyValArr = handleNestedObj(key, value)
-                for (const [key, value] of keyValArr) {
-                    rows[index].push(key)
-                    keyValMap.set(key, [...(keyValMap.get(key) ?? []), value]);
-                }
+  const matrix = [keys];
 
-            } else {
-                // primitive val
-                rows[index].push(key)
-                keyValMap.set(key, [...(keyValMap.get(key) ?? []), value]);
-            }
-        }
-    })
-    const sortedKeys = [...keyValMap.keys()].sort()
-    matrix.unshift(sortedKeys)
+  for (const obj of arr) {
+    const keyToVal: Record<string, primitive> = {};
+    getValues(obj, "", keyToVal);
 
-    rows.forEach((row, index) => {
-        row.forEach((item) => {
-            const col = sortedKeys.findIndex(key => key === item)
-            const val = keyValMap.get(item).shift()
-            matrix[index + 1][col] = val ? val : ""
-        })
-    })
+    const row: primitive[] = [];
+    keys.forEach((key) => {
+      if (key in keyToVal) {
+        row.push(keyToVal[key]);
+      } else {
+        row.push("");
+      }
+    });
+    matrix.push(row);
+  }
 
-    return matrix
+  return matrix;
 }
 
-function handleNestedObj(prefix: string, item: JSONValue[] | { [key: string]: JSONValue}) {
-    const result = []
-    for (const [key, value] of Object.entries(item)) {
-        const concatKey = [prefix, key].join(".")
-        if (value !== null && typeof value === 'object') {
-            result.push(...handleNestedObj(concatKey, value))
-        }
-        result.push([concatKey, value])
+function getKeys(obj: JSONValue, path = "") {
+  if (!isObject(obj)) {
+    return path;
+  }
+  return Object.entries(obj).flatMap(([key, value]) =>
+    getKeys(value, path ? `${path}.${key}` : key)
+  );
+}
+
+function getValues(
+  obj: JSONValue,
+  path = "",
+  keyToVal: { [key: string]: JSONValue }
+) {
+  if (Array.isArray(obj)) {
+    obj.forEach((value, index) => {
+      const newPath = path ? `${path}.${index}` : `${index}`;
+      getValues(value, newPath, keyToVal);
+    });
+  } else if (isObject(obj)) {
+    for (const key in obj) {
+      const newPath = path ? `${path}.${key}` : key;
+      getValues(obj[key], newPath, keyToVal);
     }
-    return result
+  } else {
+    keyToVal[path] = obj;
+  }
+}
+
+function isObject(obj: JSONValue): obj is { [key: string]: JSONValue } {
+  return obj !== null && typeof obj === "object";
 }
