47ebd9e5767f53b65900999f7de38fcd875c2631
newgardener <jennyshin106@gmail.com>
2024-09-08 22:58:31 +0900

    enhance: sorting part using minHeap

--- 07. intervals/leetcode/253-meeting_room_ii.py
+++ 07. intervals/leetcode/253-meeting_room_ii.py
@@ -13,17 +13,43 @@ def minMeetingRooms(self, intervals: list[list[int]]) -> int:
     ends = sorted([interval[1] for interval in intervals])
     # l for starts, r for ends
     l, r = 0, 0
-    # res: max(# of ongoing meetings)
-    res, cnt = 0, 0
+    # rooms: max(# of ongoing meetings)
+    rooms, meetings = 0, 0
     while l < len(intervals):
         # case: need a meeting room
         if starts[l] < ends[r]:
-            cnt += 1
+            meetings += 1
             l += 1
         # case: meeting ends
         else:
-            cnt -= 1
+            meetings -= 1
             r += 1
         # update max(# of ongoing meetings)
-        res = max(res, cnt)
-    return res
+        rooms = max(rooms, meetings)
+    return rooms
+
+
+# %%
+import heapq
+
+
+"""
+Min-heap approach
+ㄴ sorting using min-heap O(nlogn)
+"""
+
+
+def minMeetingRooms(self, intervals: list[list[int]]) -> int:
+    minHeap = []
+    for interval in intervals:
+        heapq.heappush(minHeap, (interval[0], 1))  # meeting starts
+        heapq.heappush(minHeap, (interval[1], -1))  # meeting ends
+
+    # rooms: max(# of ongoing meetings)
+    rooms, meetings = 0, 0
+    while minHeap:
+        meetings += heapq.heappop(minHeap)[1]
+        rooms = max(rooms, meetings)
+    return rooms
+
+
