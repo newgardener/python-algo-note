ebab0b6499c6044df30fdc12d7d403ca2ed6bac8
newgardener <jennyshin106@gmail.com>
2024-09-22 00:36:45 +0900

    chore: add notes on maxArea problem

--- 02-1. two-pointers/leetcode/11-container_with_most_water.py
+++ 02-1. two-pointers/leetcode/11-container_with_most_water.py
@@ -5,6 +5,7 @@ def maxArea(height: List[int]) -> int:
     l, r = 0, len(height) - 1
     area = 0
     while l < r:
+        # update maxArea narrowing down the search space
         area = max(area, min(height[l], height[r]) * (r - l))
         if height[l] <= height[r]:
             l += 1
