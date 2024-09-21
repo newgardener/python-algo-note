9472c9db74620ded66d40d975a1b91e70ad91204
newgardener <jennyshin106@gmail.com>
2024-09-21 22:41:18 +0900

    feat: bounded knapsack template

---
+++ 06-2. dp/concept/bounded_knapsack.py
@@ -0,0 +1,33 @@
+"""
+W: max capacity of knapsack
+weights: weight of each item
+values: value of each item
+"""
+
+
+def boundedKnapsack(W, weights, values):
+    n = len(weights)
+    # initialize dp array from 0 to W
+    dp = [0] * (W + 1)
+
+    # consider each item one by one
+    for i in range(n):
+        # we need to go backwards to avoid using the same items multiple times
+        for w in range(W, weights[i] - 1, -1):
+            dp[w] = max(dp[w], dp[w - weights[i]] + values[i])
+    return dp[W]
+
+
+def boundedKnapsack2D(W, weights, values):
+    n = len(weights)
+    dp = [[0] * (W + 1) for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        for w in range(W + 1):
+            if w >= weights[i - 1]:
+                dp[i][w] = max(
+                    dp[i - 1][w], dp[i - 1][w - weights[i - 1]] + values[i - 1]
+                )
+            else:
+                dp[i][w] = dp[i - 1][2]
+    return dp[n][W]
--- 06-2. dp/leetcode/knapsack(unbounded)/518-coin_change_ii.py
+++ 06-2. dp/leetcode/knapsack(unbounded)/518-coin_change_ii.py
@@ -11,12 +11,9 @@ class Solution:
         # i개의 동전을 이용해 금액 0을 만드는 방법 => 동전 안쓰기
         for i in range(n + 1):
             dp[i][0] = 1
-        # i개의 동전을 이용해 금액 0을 만드는 방법 => 없음
-        for j in range(amount + 1):
-            dp[0][j] = 0
 
         for i in range(1, n + 1):
-            for w in range(1, amount + 1):
+            for w in range(amount + 1):
                 if w >= coins[i - 1]:
                     dp[i][w] = dp[i - 1][w] + dp[i][w - coins[i - 1]]
                 else:
