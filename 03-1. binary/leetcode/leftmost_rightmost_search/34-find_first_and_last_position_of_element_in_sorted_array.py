00c94cb978310b450a8ee81b964369b332a3be90
newgardener <jennyshin106@gmail.com>
2024-09-25 20:27:44 +0900

    solve: leftmost, rightmost binary search problem

---
+++ 03-1. binary/leetcode/leftmost_rightmost_search/34-find_first_and_last_position_of_element_in_sorted_array.py
@@ -0,0 +1,30 @@
+"""
+Leftmost Search: O(logN)
+Rightmost Search O(logN)
+"""
+
+
+def searchRange(nums: list[int], target: int) -> list[int]:
+    if not nums:
+        return [-1, -1]
+
+    # leftmost search
+    l, r = 0, len(nums)
+    while l < r:
+        mid = l + (r - l) // 2
+        if nums[mid] >= target:
+            r = mid
+        else:
+            l = mid + 1
+    leftmost = l if l < len(nums) and nums[l] == target else -1
+
+    # rightmost search
+    l, r = 0, len(nums)
+    while l < r:
+        mid = l + (r - l) // 2
+        if nums[mid] > target:
+            r = mid
+        else:
+            l = mid + 1
+    rightmost = l - 1 if l - 1 < len(nums) and nums[l - 1] == target else -1
+    return [leftmost, rightmost]
