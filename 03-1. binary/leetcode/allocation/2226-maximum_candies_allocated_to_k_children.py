4c30ea330a368f3f62dde3a76a97bcfe714d35b8
newgardener <jennyshin106@gmail.com>
2024-09-29 16:34:03 +0900

    solve: binary allocation type of problem

---
+++ 03-1. binary/leetcode/allocation/2226-maximum_candies_allocated_to_k_children.py
@@ -0,0 +1,18 @@
+def maximumCandies(candies: list[int], k: int) -> int:
+    total = sum(candies)
+    if total < k:
+        return 0
+
+    l, r = 1, max(candies) + 1
+    max_candy = 0
+    while l < r:
+        mid = l + (r - l) // 2
+        piles = 0
+        for candy in candies:
+            piles += candy // mid
+        if piles < k:
+            r = mid
+        else:
+            max_candy = max(max_candy, mid)
+            l = mid + 1
+    return max_candy
