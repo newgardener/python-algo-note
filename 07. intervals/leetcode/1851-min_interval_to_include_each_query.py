60e67b5c8deb37e9da5b26fb887f75a19184482b
newgardener <jennyshin106@gmail.com>
2024-10-04 01:09:42 +0900

    solve: minimum interval (hard) problem using minHeap

--- 07. intervals/leetcode/1851-min_interval_to_include_each_query.py
+++ 07. intervals/leetcode/1851-min_interval_to_include_each_query.py
@@ -1,38 +1,25 @@
-from collections import defaultdict
-from typing import List
-
-"""
-Why it is not working?
-edge case found when 
-intervals=[[6,6],[5,5],[10,10],[3,6],[9,9],[7,7],[2,10],[5,5],[3,7],[10,10]],
-queries=[1,8,9,1,8,3,9,3,10,1]
-=> by binary search, we cannot get to the smallest target interval
-"""
+import heapq
 
 
-class Solution:
-    def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
-        n = len(queries)
-        intervals.sort(key=lambda x: x[0])
-        size = [item[1] - item[0] + 1 for item in intervals]
+def minInterval(intervals: list[list[int]], queries: list[int]) -> list[int]:
+    # sort intervals by its startTime
+    intervals.sort()
+    sizes = [r - l + 1 for l, r in intervals]
 
-        def process(query):
-            # find leftmost interval
-            left, right = 0, len(intervals) - 1
-            while left <= right:
-                mid = (left + right) // 2
-                if intervals[mid][1] < query:
-                    left = mid + 1
-                else:
-                    right = mid - 1
+    minHeap = []
+    res = {}
+    i = 0
+    for q in sorted(queries):
+        # if startTime of intervals[i] is smaller or equal to q, we can insert to minHeap
+        # minHeap manages (size, endTime of interval) to pop the smallest possible interval
+        while i < len(intervals) and intervals[i][0] <= q:
+            heapq.heappush(minHeap, (sizes[i], intervals[i][1]))
+            i += 1
 
-            minSize = float('infinity')
-            for i in range(left, n):
-                start, end = intervals[i]
-                if start > query:
-                    break
-                if start <= query <= end:
-                    minSize = min(minSize, size[i])
-            return minSize if minSize != float('infinity') else -1
+        # pop all invalid intervals where endTime is smaller than q
+        while minHeap and minHeap[0][1] < q:
+            heapq.heappop(minHeap)
 
-        return [process(q) for q in queries]
+        res[q] = minHeap[0][0] if minHeap else -1
+
+    return [res[q] for q in queries]
