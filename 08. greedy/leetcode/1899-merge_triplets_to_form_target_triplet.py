96b9f78f57958402b378de640659c4da05e3443d
newgardener <jennyshin106@gmail.com>
2024-10-03 16:14:50 +0900

    review: merge triplets to form target triplet

--- 08. greedy/leetcode/1899-merge_triplets_to_form_target_triplet.py
+++ 08. greedy/leetcode/1899-merge_triplets_to_form_target_triplet.py
@@ -10,10 +10,12 @@ Time Complexity: O(N)
 
 
 def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+    # we first filter triplets in a way each item should be less than or equal to its corresponding target
     filtered = [item for item in triplets if item[0] <= target[0] and item[1] <= target[1] and item[2] <= target[2]]
     if not filtered:
         return False
 
+    # we can greedily check whether each item matches with target
     good = set()
     for item in filtered:
         for i, v in enumerate(item):
