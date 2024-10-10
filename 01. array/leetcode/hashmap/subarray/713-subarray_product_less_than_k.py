c645d18db00d326289afd97282d1d91394d3e978
newgardener <jennyshin106@gmail.com>
2024-10-10 22:43:12 +0900

    review: subarray product less than k (counting subarray r - l + 1)

---
+++ 01. array/leetcode/hashmap/subarray/713-subarray_product_less_than_k.py
@@ -0,0 +1,18 @@
+# count the number of subarray product of each element is less than k
+def numSubarrayProductLessThanK(nums: list[int], k: int) -> int:
+    cnt = 0
+    subProduct = 1
+    l = 0
+    for r, num in enumerate(nums):
+        subProduct *= num
+        if subProduct < k:
+            cnt += r - l + 1  # ex. [1,2,3] all possible subarray is [1], [1,2], [1,2,3]
+        else:
+            while l < r and subProduct >= k:
+                subProduct //= nums[l]
+                l += 1
+
+            if subProduct < k:
+                cnt += r - l + 1
+
+    return cnt
