d68cf3d50bdbb00c9ca104d553f37ce3f2fdc8a6
newgardener <jennyshin106@gmail.com>
2024-10-13 00:37:34 +0900

    enhance: simplify 3x3 filter solution

--- 14. matrix/leetcode/2373-largest_local_values_in_a_matrix.py
+++ 14. matrix/leetcode/2373-largest_local_values_in_a_matrix.py
@@ -2,17 +2,13 @@
 def largestLocal(grid: list[list[int]]) -> list[list[int]]:
     n = len(grid)
     matrix = []
-    r = 0
-    while r + 3 <= n:
+    for r in range(n - 2):
         row = []
-        c = 0
-        while c + 3 <= n:
+        for c in range(n - 2):
             localMax = 0
             for i in range(r, r + 3):
                 for j in range(c, c + 3):
                     localMax = max(localMax, grid[i][j])
             row.append(localMax)
-            c += 1
         matrix.append(row)
-        r += 1
     return matrix
