5f411cf603cdc9fd11c2af923c11ced45a31d6da
newgardener <jennyshin106@gmail.com>
2024-11-23 16:41:38 +0900

    review: bst - minDepth and maxDepth

--- 04. trees/leetcode/depth/104-maximum_depth_of_binary_tree.py
+++ 04. trees/leetcode/depth/104-maximum_depth_of_binary_tree.py
@@ -7,11 +7,14 @@ class TreeNode:
         self.left = left
         self.right = right
 
-def maxDepth(self, root: Optional[TreeNode]) -> int:
+
+# maxDepth -> dfs
+def maxDepth(root: Optional[TreeNode]) -> int:
     def dfs(node):
         if not node:
             return 0
         left = dfs(node.left)
         right = dfs(node.right)
         return max(left, right) + 1
+
     return dfs(root)
--- 04. trees/leetcode/depth/111-minimum_depth_of_binary_tree.py
+++ 04. trees/leetcode/depth/111-minimum_depth_of_binary_tree.py
@@ -9,19 +9,19 @@ class TreeNode:
         self.right = right
 
 
-class Solution:
-    def minDepth(self, root: Optional[TreeNode]) -> int:
-        depth = 0
-        if not root:
-            return depth
-
-        q = collections.deque([(root, 1)])
-        while q:
-            node, depth = q.popleft()
-            if not node.left and not node.right:
-                break
-            if node.left:
-                q.append((node.left, depth + 1))
-            if node.right:
-                q.append((node.right, depth + 1))
+# minDepth -> bfs
+def minDepth(root: Optional[TreeNode]) -> int:
+    depth = 0
+    if not root:
         return depth
+
+    q = collections.deque([(root, 1)])
+    while q:
+        node, depth = q.popleft()
+        if not node.left and not node.right:
+            break
+        if node.left:
+            q.append((node.left, depth + 1))
+        if node.right:
+            q.append((node.right, depth + 1))
+    return depth
