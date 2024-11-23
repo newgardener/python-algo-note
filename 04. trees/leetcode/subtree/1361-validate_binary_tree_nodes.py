791774ac2fe6ac7ef37f47c91352d1d04536d08f
newgardener <jennyshin106@gmail.com>
2024-11-23 21:58:02 +0900

    review: validate binary tree nodes problem

--- 04. trees/leetcode/subtree/1361-validate_binary_tree_nodes.py
+++ 04. trees/leetcode/subtree/1361-validate_binary_tree_nodes.py
@@ -4,25 +4,28 @@ from collections import deque
 def validateBinaryTreeNodes(
     n: int, leftChild: list[int], rightChild: list[int]
 ) -> bool:
-    children = set(leftChild + rightChild)
-    children.discard(-1)
-    # no root case
-    if len(children) == n:
+    indegree = [0] * n
+    for i in range(n):
+        left = leftChild[i]
+        right = rightChild[i]
+        if left != -1:
+            indegree[left] += 1
+        if right != -1:
+            indegree[right] += 1
+
+    root = [i for i, d in enumerate(indegree) if d == 0]
+    # BST must have one root node
+    if not root or len(root) > 1:
         return False
 
-    root = next(i for i in range(n) if i not in children)
-    visited = [False] * n
-    visited[root] = True
-    q = deque([root])
+    q = deque(root)
     while q:
         node = q.popleft()
         for child in [leftChild[node], rightChild[node]]:
-            if child == -1:
-                continue
-            # cycle detection
-            if visited[child]:
-                return False
-            visited[child] = True
-            q.append(child)
-    # at the end, if all nodes are visited then it is valid binary tree nodes
-    return all(visited)
+            if child != -1:
+                indegree[child] -= 1
+                if indegree[child] > 0:
+                    return False
+                q.append(child)
+    # check if all node's indegree turns 0
+    all(d == 0 for d in indegree)
