5b144c2e5c7e9b30bbf19010de737dc8a14990e3
newgardener <jennyshin106@gmail.com>
2025-02-02 21:28:39 +0900

    review: car fleet problem
    - analyze merging cars technique (slope of linear chart)

--- 02-2. stack-queue/leetcode/853-car_fleet.py
+++ 02-2. stack-queue/leetcode/853-car_fleet.py
@@ -1,13 +1,24 @@
-from typing import List
+"""
+Time Complexity: O(NlogN) -> sorting
+Space Complexity: O(N) -> stack
+"""
 
 
-def carFleet(target: int, position: List[int], speed: List[int]) -> int:
+def carFleet(target: int, position: list[int], speed: list[int]) -> int:
     pairs = [[p, s] for p, s in zip(position, speed)]
+    pairs.reverse()
 
     stack = []
-    for p, s in sorted(pairs)[::-1]:
-        stack.append((target - p) / s)
-        if len(stack) >= 2 and stack[-2] >= stack[-1]:
-            stack.pop()
+    # iterate pairs in a position-descending order
+    for p, s in pairs:
+        slope = (target - p) / s
+        # merge cars if prev_slope is smaller or equal to cur_slope
+        # it means there would be a car fleet
+        if not stack or stack[-1] < slope:
+            stack.append(slope)
 
+    # what remains in the stack is the distinct number of car fleet
     return len(stack)
+
+
+print(carFleet(12, [10, 8, 0, 5, 3], [2, 4, 1, 1, 3]))
