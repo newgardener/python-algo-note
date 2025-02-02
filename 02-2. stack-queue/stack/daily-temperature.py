2e3c5abc357a4b1a98eb131970bfa90afc320716
newgardener <jennyshin106@gmail.com>
2025-02-02 20:11:41 +0900

    review: daily temperature 3 ways (in order, in reversed, in dp)

--- 02-2. stack-queue/stack/daily-temperature.py
+++ 02-2. stack-queue/stack/daily-temperature.py
@@ -1,7 +1,12 @@
 T = [73, 74, 75, 71, 69, 72, 76, 73]
+"""
+Time Complexity: O(N)
+Space Complexity for stack: O(N)
+Space Complexity for DP: O(1)
+"""
 
 
-def dailyTemperature(T):
+def dailyTemperatureInOrder(T):
     answer = [0] * len(T)
     stack = []
     for i, cur in enumerate(T):
@@ -12,4 +17,35 @@ def dailyTemperature(T):
     return answer
 
 
-print(dailyTemperature(T))
+print(dailyTemperatureInOrder(T))
+
+def dailyTemperatureInReversed(T):
+    answer = [0] * len(T)
+    stack = []
+    for i in range(len(T)-1, -1, -1):
+        temp = T[i]
+        while stack and temp >= T[stack[-1]]:
+            stack.pop()
+        answer[i] = stack[-1] - i if stack else 0
+        stack.append(i)
+    return answer
+
+print(dailyTemperatureInReversed(T))
+
+def dailyTemperatureDP(T):
+    n = len(T)
+    answer = [0] * len(T)
+    for i in range(n-2, -1, -1):
+        j = i + 1
+        while j < n and T[j] <= T[i]:
+            if answer[j] == 0:
+                j = n # in order to denote we don't need to update answer[i]
+                break
+            j += answer[j]
+
+        if j < n:
+            answer[i] = j - i
+    return answer
+
+print(dailyTemperatureDP(T))
+
