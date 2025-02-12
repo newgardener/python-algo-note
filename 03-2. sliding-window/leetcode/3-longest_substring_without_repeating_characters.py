0c385720aa98f503e5f0c6f44986824dd8c204c0
newgardener <jennyshin106@gmail.com>
2025-02-12 19:06:55 +0900

    feat: solve and analyze the length of longest substring problem

--- 03-2. sliding-window/leetcode/3-longest_substring_without_repeating_characters.py
+++ 03-2. sliding-window/leetcode/3-longest_substring_without_repeating_characters.py
@@ -1,17 +1,44 @@
+"""
+n = length of string s, m = the number of unique character in string s
+Time Complexity: O(n)
+Space Complexity: O(m)
+"""
+
+# %%
+# solution using set
+
+
 def lengthOfLongestSubstring(s: str) -> int:
-    maxLength = 0
-    window = set()
+    charSet = set()
     l = 0
-    for r, ch in enumerate(s):
-        if ch not in window:
-            window.add(ch)
-        else:
-            while s[l] != ch:
-                window.remove(s[l])
-                l += 1
+    longest = 0
+    for r in range(len(s)):
+        ch = s[r]
+        # ensure not to add duplicate character into charSet
+        while ch in charSet:
+            charSet.remove(s[l])
             l += 1
-        maxLength = max(maxLength, r - l + 1)
-    return maxLength
+        charSet.add(ch)
+        # calculate the length of longest, non-duplicate string every time
+        longest = max(longest, r - l + 1)
+    return longest
 
 
+# %%
+# solution using hashmap
 
+
+def lengthOfLongestSubstring(s: str) -> int:
+    # record index of each character
+    charMap = {}
+    l = 0
+    longest = 0
+    for r in range(len(s)):
+        ch = s[r]
+        # in order to retrieve duplicating character index right away: O(1)
+        if ch in charMap:
+            l = max(l, charMap[ch] + 1)
+        charMap[ch] = r
+        # calculate the length of longest, non-duplicate string every time
+        longest = max(longest, r - l + 1)
+    return longest
