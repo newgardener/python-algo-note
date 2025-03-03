84374be629bcb8789396751201fb4a10f0e0f64b
newgardener <jennyshin106@gmail.com>
2025-03-03 12:39:58 +0900

    fix: logic of merge intervals solution

--- sort/leetcode/merge-sort/56-merge_intervals.py
+++ sort/leetcode/merge-sort/56-merge_intervals.py
@@ -7,9 +7,11 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort(key=lambda x: x[0])
         result = []
-        for interval in intervals:
-            if not result or result[-1][1] < interval[0]:
-                result.append(interval)
+
+        for start, end in intervals:
+            lastEnd = result[-1][1]
+            if start <= lastEnd:
+                result[-1][1] = max(lastEnd, end)
             else:
-                result[-1][1] = max(result[-1][1], interval[1])
+                result.append([start, end])
         return result
