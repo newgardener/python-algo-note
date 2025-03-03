d302380d0a0f569e789789caf4accadec4fad01c
newgardener <jennyshin106@gmail.com>
2025-03-03 14:08:22 +0900

    review: sliding window maximum - maxHeap, monotonic decreasing queue solution

--- 03-2. sliding-window/leetcode/239-sliding_window_maximum.py
+++ 03-2. sliding-window/leetcode/239-sliding_window_maximum.py
@@ -1,13 +1,38 @@
-from collections import deque
-from typing import List
+# %%
+"""
+MaxHeap
+Time Complexity: O(NlogN)
+Space Complexity: O(N)
+"""
+
+import heapq
+
+
+def maxSlidingWindow(nums: list[int], k: int) -> list[int]:
+    result = []
+    heap = []
+    for i in range(len(nums)):
+        heapq.heappush(heap, (-nums[i], i))
+        # shrink window size to k
+        while heap[0][1] <= i - k:
+            heapq.heappop(heap)
+        if i >= k - 1:
+            result.append(-heap[0][0])
+    return result
+
+
+# %%
 
 """
 Monotonic Decreasing Queue
 Time Complexity: O(N)
+Space Complexity: O(N)
 """
 
+from collections import deque
 
-def maxSlidingWindow(nums: List[int], k: int) -> List[int]:
+
+def maxSlidingWindow(nums: list[int], k: int) -> list[int]:
     result = []
     q = deque()
 
@@ -15,7 +40,6 @@ def maxSlidingWindow(nums: List[int], k: int) -> List[int]:
         # monotonic decreasing queue
         while q and nums[q[-1]] < nums[r]:
             q.pop()
-
         # store the index of num
         q.append(r)
 
@@ -23,7 +47,8 @@ def maxSlidingWindow(nums: List[int], k: int) -> List[int]:
         if r - k == q[0]:
             q.popleft()
 
-        if r + 1 >= k:
+        # first element of the queue is the largest number within the window
+        if r >= k - 1:
             result.append(nums[q[0]])
 
     return result
